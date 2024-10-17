fcbf21d239ba5e57e39359196284532105f6a94c
Try Ajitiono <try.ajitiono@accelbyte.net>
2024-10-17 11:37:10 +0700

    add example login website demo

--- examples/sdk-demo-example/src/demo/components/Snippet.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react'
-
-export function Snippet({ children }: { children: ReactNode }) {
-  return (
-    <pre className="max-h-[600px] w-full border border-slate-500 bg-slate-200 rounded p-2 overflow-y-auto whitespace-pre text-xs">
-      {children}
-    </pre>
-  )
-}
